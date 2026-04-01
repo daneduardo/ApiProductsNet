a5c9e17861558f593ceb7eaf5c6c6c2280c0619a
daneduardo <dan.eduardo@gmail.com>
2026-04-01 13:36:26 -0600

    feat: Project .net api

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# 1. Etapa de compilación (SDK)
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /app
+
+# Copiar archivos de proyecto y restaurar dependencias
+COPY ["src/api/ProductsApi.csproj", "src/api/"]
+RUN dotnet restore "src/api/ProductsApi.csproj"
+
+# Copiar el resto del código y compilar
+COPY . .
+WORKDIR "/app/src/api"
+RUN dotnet publish "ProductsApi.csproj" -c Release -o /app/publish
+
+# 2. Etapa de ejecución (Runtime)
+FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS runtime
+WORKDIR /app
+COPY --from=build /app/publish .
+
+# Exponer el puerto 8080 como solicita el requerimiento
+ENV ASPNETCORE_URLS=http://+:8080
+EXPOSE 8080
+
+ENTRYPOINT ["dotnet", "ProductsApi.dll"]
---
+++ src/test/ProductApi.Tests/ProductsControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductsApi.Controllers;
+using ProductsApi.Data;
+using ProductsApi.Models;
+using System.Reflection.Metadata;
+using Xunit;
+
+namespace ProductsApi.Tests;
+
+public class ProductsControllerTests
+{
+    private readonly ProductsController _controller;
+    private readonly ProductDbContext _context;
+
+    public ProductsControllerTests()
+    {
+        var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+        _context = new ProductDbContext(options);
+        _controller = new ProductsController(_context);
+    }
+    [Fact]
+    public async Task CreateProduct_ShouldReturnCreated_WhenValid()
+    {
+        var product = new Product
+        {
+            Name = "Monitor Gamer",
+            Price = 350,
+            Stock = 10,
+            Description = "4K 144Hz"
+        };
+        var result = await _controller.CreateProduct(product);
+
+        var createdResult = result.Result.Should().BeOfType<CreatedAtActionResult>().Subject;
+        var returnedProduct = createdResult.Value.Should().BeOfType<Product>().Subject;
+
+        returnedProduct.Name.Should().Be("Monitor Gamer");
+        _context.Products.Count().Should().Be(1);
+    }
+    [Fact]
+    public async Task GetProducts_ShouldReturnAllProducts()
+    {
+        _context.Products.Add(new Product { Name = "Mouse", Price = 10, Stock = 10 });
+        _context.Products.Add(new Product { Name = "Monitor", Price = 100, Stock = 10 });
+        await _context.SaveChangesAsync();
+
+        var result = await _controller.GetProduct();
+
+        var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+        var products = okResult.Value.Should().BeAssignableTo<IEnumerable<Product>>().Subject;
+
+        products.Count().Should().Be(2);
+    }
+    [Fact]
+    public async Task DeleteProduct_ShouldRemoveFromDb()
+    {
+        var product = new Product { Id = Guid.NewGuid(), Name = "To Delete", Price = 10 };
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        await _controller.DeleteProduct(product.Id);
+
+        _context.Products.Should().BeEmpty();
+    }
+}
